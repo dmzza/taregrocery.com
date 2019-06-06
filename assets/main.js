9cae9ef089c2411443a7d8acc1de90ef395145dc
David Mazza <davidedwardmazza@gmail.com>
2019-06-06 16:38:25 -0700

    bound images within the bottom/left viewport edges

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,30 @@ window.onload = function() {
         link.onmouseover = function(event) {
             let imageId = link.dataset.imageId
             let image = document.getElementById(imageId)
-            image.style = "display:block;"
+            image.style.display = "block"
+
+            let boundDimensionByViewport = function(elementStart, elementDimension, viewportDimension, margin) {
+                let elementEnd = elementStart + elementDimension
+                var newElementStart = elementStart
+                if(elementEnd > viewportDimension - margin) {
+                    newElementStart -= elementEnd - (viewportDimension - margin)
+                }
+                return newElementStart
+            }
+
+            let linkRect = link.getBoundingClientRect()
+            let imageRect = image.getBoundingClientRect()
+            let viewportHeight = window.innerHeight
+            let viewportWidth = window.innerWidth
+            var imageTop = linkRect.bottom
+            var imageLeft = linkRect.left
+            let margin = 30
+
+            imageTop = boundDimensionByViewport(imageTop, imageRect.height, viewportHeight, margin)
+            imageLeft = boundDimensionByViewport(imageLeft, imageRect.width, viewportWidth, 15)
+
+            image.style.top = "" + imageTop + "px"
+            image.style.left = "" + imageLeft + "px"
         }
     
         link.onmouseout = function(event) {
@@ -13,11 +36,11 @@ window.onload = function() {
             let x = event.clientX
             let y = event.clientY
             let elementMouseIsOver = document.elementFromPoint(x, y);
-            if(elementMouseIsOver !== null && elementMouseIsOver.parentElement.id !== imageId) {
-                image.style = "display:none;"
+            if(elementMouseIsOver !== null && elementMouseIsOver.id !== imageId) {
+                image.style.display = "none"
             } else {
                 image.onmouseout = function(event) {
-                    image.style = "display:none;"
+                    image.style.display = "none"
                 }
             }
         }
