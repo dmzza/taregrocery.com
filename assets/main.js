2f5a0727881a7f68c088c1a9820b906e347f775a
David Mazza <davidedwardmazza@gmail.com>
2019-06-06 23:18:12 -0700

    toggle highlighted class instead of setting color

--- assets/main.js
+++ assets/main.js
@@ -5,10 +5,9 @@ window.onload = function() {
             let imageId = link.dataset.imageId
             let image = document.getElementById(imageId)
             let companyName = document.getElementById("company-name")
-            let primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color');
             image.style.display = "block"
-            link.style.color = primaryColor
-            companyName.style.color = "white"
+            link.classList.add(["highlighted"])
+            companyName.classList.remove(["highlighted"])
 
             let boundDimensionByViewport = function(elementStart, elementDimension, viewportDimension, minimumSpaceFromEdge) {
                 let elementEnd = elementStart + elementDimension
@@ -38,14 +37,13 @@ window.onload = function() {
             let imageId = link.dataset.imageId
             let image = document.getElementById(imageId)
             let companyName = document.getElementById("company-name")
-            let primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color');
             let x = event.clientX
             let y = event.clientY
             let elementMouseIsOver = document.elementFromPoint(x, y);
             let dismissImage = function(event) {
                 image.style.display = "none"
-                link.style.color = "white"
-                companyName.style.color = primaryColor
+                link.classList.remove(["highlighted"])
+                companyName.classList.add(["highlighted"])
             }
             if(elementMouseIsOver !== null && elementMouseIsOver.id !== imageId) {
                 dismissImage(event)
